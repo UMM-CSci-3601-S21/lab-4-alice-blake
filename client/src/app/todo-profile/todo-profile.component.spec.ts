833de199ff7222d7c2887cca7335a5af4692754c
Blake Johnson <joh18484@morris.umn.edu>
2021-03-04 13:07:10 -0600

    Finished Tests for Todo Card and Profile
    Finished creating tests for the todo card and profile to cover all cases of the code. Works great!

--- client/src/app/todo-profile/todo-profile.component.spec.ts
+++ client/src/app/todo-profile/todo-profile.component.spec.ts
@@ -1,17 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRouteStub } from 'src/testing/activated-route-stub';
+import { MockTodoService } from 'src/testing/todo.service.mock';
+import { TodoCardComponent } from '../todo-card/todo-card.component';
+import { Todo } from '../todo/todo';
+import { TodoService } from '../todo/todo.service';
 
 import { TodoProfileComponent } from './todo-profile.component';
 
 describe('TodoProfileComponent', () => {
   let component: TodoProfileComponent;
   let fixture: ComponentFixture<TodoProfileComponent>;
+  const activatedRoute: ActivatedRouteStub = new ActivatedRouteStub();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TodoProfileComponent ]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        MatCardModule
+      ],
+      declarations: [ TodoProfileComponent, TodoCardComponent ],
+      providers: [
+        {provide: TodoService, useValue: new MockTodoService() },
+        {provide: ActivatedRoute, useValue: activatedRoute }
+      ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoProfileComponent);
@@ -19,8 +36,45 @@ describe('TodoProfileComponent', () => {
     fixture.detectChanges();
   });
 
-  //it('should create', () => {
-   // expect(component).toBeTruthy();
-  //});
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should navigate to a specific todo profile', () => {
+    const expectedTodo: Todo = MockTodoService.testTodos[0];
+    // Setting this should cause anyone subscribing to the paramMap
+    // to update. Our `UserProfileComponent` subscribes to that, so
+    // it should update right away.
+    activatedRoute.setParamMap({ id: expectedTodo._id });
+
+    expect(component.id).toEqual(expectedTodo._id);
+    expect(component.todo).toEqual(expectedTodo);
+  });
+
+  it('should navigate to correct todo when the id parameter changes', () => {
+    let expectedTodo: Todo = MockTodoService.testTodos[0];
+    // Setting this should cause anyone subscribing to the paramMap
+    // to update. Our `UserProfileComponent` subscribes to that, so
+    // it should update right away.
+    activatedRoute.setParamMap({ id: expectedTodo._id });
+
+    expect(component.id).toEqual(expectedTodo._id);
+
+    // Changing the paramMap should update the displayed user profile.
+    expectedTodo = MockTodoService.testTodos[1];
+    activatedRoute.setParamMap({ id: expectedTodo._id });
+
+    expect(component.id).toEqual(expectedTodo._id);
+  });
+
+  it('should have `null` for the todo for a bad ID', () => {
+    activatedRoute.setParamMap({ id: 'badID' });
+
+    // If the given ID doesn't map to a user, we expect the service
+    // to return `null`, so we would expect the component's user
+    // to also be `null`.
+    expect(component.id).toEqual('badID');
+    expect(component.todo).toBeNull();
+  });
 
 });
--- client/src/testing/todo.service.mock.ts
+++ client/src/testing/todo.service.mock.ts
@@ -12,21 +12,21 @@ export class MockTodoService extends TodoService {
   static testTodos: Todo[] = [
     {
       _id: 'chris_id',
-      owner: 'chris',
+      owner: 'Chris',
       status: true,
       body: 'string',
       category: 'test todos',
     },
     {
       _id: 'pat_id',
-      owner: 'pat',
+      owner: 'Pat',
       status: false,
       body: 'string',
       category: 'test todos',
     },
     {
       _id: 'jamie_id',
-      owner: 'jamie',
+      owner: 'Jamie',
       status: true,
       body: 'string',
       category: 'test todos',
@@ -37,8 +37,19 @@ export class MockTodoService extends TodoService {
     super(null);
   }
 
-  getTodos(): Observable<Todo[]> {
+  getTodos(filters: {owner?: string; status?: boolean; category?: string; body?: string}): Observable<Todo[]> {
     return of(MockTodoService.testTodos);
+  }
+
+  getTodosById(id: string): Observable<Todo> {
+    // If the specified ID is for the first test todo,
+    // return that todo, otherwise return `null` so
+    // we can test illegal user requests.
+    if (id === MockTodoService.testTodos[0]._id) {
+      return of (MockTodoService.testTodos[0]);
+    } else {
+      return of (null);
+    }
   }
 
 }
